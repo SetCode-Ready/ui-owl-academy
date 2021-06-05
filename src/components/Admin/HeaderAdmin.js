8b15a44fbfa855ff6837afaf433d057a2b0a6a8c
cicerohss <cicerohenrique282@gmail.com>
2021-06-04 23:13:14 -0300

    Add Image Perfil and Responsive Design

--- src/components/Admin/HeaderAdmin.js
+++ src/components/Admin/HeaderAdmin.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import style from './HeaderAdmin.module.css'
+import perfil from '../../Assets/perfil.jpg'
 import {ReactComponent as Logo} from '../../Assets/Owl-HEAD.svg'
 import {FiBell} from 'react-icons/fi'
 import {FiLogOut} from 'react-icons/fi'
@@ -7,20 +8,48 @@ import {IconContext} from 'react-icons'
 
 export default function HeaderAdmin() {
     return (
-        <header className={style.ContainerHeader}>
-            <Logo/>
-            <div className={style.ContainerUser}>
-                <IconContext.Provider value={{color: 'white', size: '1.5rem', style:{flexGrow: 2}}}>
-                    <FiBell/>
-                </IconContext.Provider>
-                <div className={style.ContainerLogin} > 
-                    <div className={style.UserPhoto}/>
-                    <p>Lula</p>
+        <header className={style.ContainerHeaderBG}>
+
+            <section className={style.ContainerHeaderAdm}>
+
+                <div className={style.LogoHeaderAdm}>
+                    <a href="/admin" className={style.Logo}><Logo/></a>
                 </div>
-                <IconContext.Provider value={{color: 'white', size: '1.5rem', style:{flexGrow: 1}}}>
-                    <FiLogOut/>
-                </IconContext.Provider>
-            </div>
+
+                <div className={style.ContainerUserAdm}>
+
+                    <ul>
+                        <li>
+                            <a href="/admin" className={style.iconAdm}>
+                                <IconContext.Provider value={{color: 'white', size: '1.5rem', style:{flexGrow: 2}}}>
+                                    <FiBell/>
+                                </IconContext.Provider>
+                            </a>
+                        </li>
+
+                            
+                        <li>
+                            <a href="/admin">
+                                <div className={style.ContainerLoginAdm}>
+                                    <img src={perfil} alt="Minha Figura"/>
+                                    <p>Cícero</p>
+                                </div>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="/admin" className={style.iconAdm}>
+                                <IconContext.Provider value={{color: 'white', size: '1.5rem', style:{flexGrow: 1}}}>
+                                    <FiLogOut/>
+                                </IconContext.Provider>
+                            </a>
+                        </li>
+                    </ul>
+                    
+                </div>
+
+            </section>
+
         </header>
     )
 }

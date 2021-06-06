d59cc8b58bc5a88bbc2754c33449ff208fc329ee
kauecdev <kaue_cavalcante.cnt@outlook.com>
2021-06-06 16:17:54 -0300

    Added api config

--- src/components/Admin/AddClass.js
+++ src/components/Admin/AddClass.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import HeaderAdmin from './HeaderAdmin'
 import style from './addClass.module.css'
 
-export default function addClass() {
+export default function AddClass() {
     return (
         <>
             <HeaderAdmin/>
